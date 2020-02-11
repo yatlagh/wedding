ce065fa94b2f87bb8d46770d4294fce2ee971952
Yassine atlagh <yassineatlagh@Yassines-iMac.home>
2020-02-11 00:22:26 -0500

    new

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCbIAsd1I8Nceeu5Zd9G_-FtKFheWUUHFo",
+    authDomain: "wedding202-b1190.firebaseapp.com",
+    databaseURL: "https://wedding202-b1190.firebaseio.com",
+    projectId: "wedding202-b1190",
+    storageBucket: "wedding202-b1190.appspot.com",
+    messagingSenderId: "696334395874",
+    appId: "1:696334395874:web:5681ebdbfd202f3cd0b1bb"
+  };
+
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Reference messages collection
+var firestore = firebase.firestore();
+
+const submitBtn = document.querySelector('#submit');
+
+let userName = document.querySelector('#userFullName');
+let userEmail = document.querySelector('#userEmail');
+let userMessage = document.querySelector('#userMessage');
+
+
+const db = firestore.collection("contactData");
+
+submitBtn.addEventListener('click', function() {
+let userNameInput = userName.value;
+let userEmailInput = userEmail.value;
+let userMessageInput = userMessage.value;
+
+db.doc()
+.set({
+  name: userNameInput,
+  email: userEmailInput,
+  message: userMessageInput
+
+})
+.then(function() {
+console.log("Data Saved");
+})
+.catch(function(error) {
+  console.log(error);
+});
+});
